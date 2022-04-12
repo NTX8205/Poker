dcd4c5283678f6c3b7a90070fe9cdb9d32b59adb
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-04-12 11:36:38 +0800

    fin

--- app/src/main/java/com/example/poker/MainActivity.kt
+++ app/src/main/java/com/example/poker/MainActivity.kt
@@ -6,25 +6,44 @@ import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import com.example.poker.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity() , View.OnTouchListener, GestureDetector.OnGestureListener{
 
     private lateinit var gDetector: GestureDetector
+    private lateinit var binding: ActivityMainBinding
+
+    var color:String ="" //花色
+    var number :Int = 1 //點數
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        img.setOnTouchListener(this)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.img.setOnTouchListener(this)
+        binding.text.text = "撲克牌魔術"
         gDetector = GestureDetector(this, this)
     }
 
     override fun onTouch(v:View?, event: MotionEvent?): Boolean {
         if (event?.action == MotionEvent.ACTION_DOWN){
-            text.text = "手指按下"
+//            binding.text.text = "手指按下"
+            color = ""
         }
-        if (event?.action == MotionEvent.ACTION_UP){
-            text.text = "手指彈開"
+        else if (event?.action == MotionEvent.ACTION_UP){
+//            binding.text.text = "手指彈開"
+            if(color==""){
+                binding.img.setImageResource(R.drawable.joker)
+            }
+            else{
+                var res:Int = getResources().getIdentifier(color + number.toString(),
+                    "drawable", getPackageName())
+                binding.img.setImageResource(res)
+            }
+
         }
         gDetector.onTouchEvent(event)
         return true
@@ -43,7 +62,30 @@ class MainActivity : AppCompatActivity() , View.OnTouchListener, GestureDetector
     }
 
     override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
-        text.text = "拖曳"
+        //        binding.text.text = "拖曳"
+        if(p0!!.x>=p1!!.x){
+            if(p0!!.y >= p1!!.y){ //左上
+                color = "c"
+            }
+            else {  //左下
+                color = "d"
+            }
+        }
+        else {
+            if(p0!!.y >= p1!!.y){   //右上
+                color = "h"
+            }
+            else {
+                color = "s"
+            }
+        }
+
+        //處理點數
+        number =abs(p0!!.y.toInt() - p1!!.y.toInt())/(binding.img.height/26)+1
+        if(number>13){
+            number=13
+        }
+
         return true
     }
 
