6e787bff8734b01c559270019b4b1dbd0615ffc8
NTX8205 <70540067+NTX8205@users.noreply.github.com>
2022-04-12 10:24:54 +0800

    v2

--- app/src/main/java/com/example/poker/MainActivity.kt
+++ app/src/main/java/com/example/poker/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.poker
 
+import android.gesture.Gesture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnTouchListener, GestureDetector.OnGestureListener{
+
+    private lateinit var gDetector: GestureDetector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        img.setOnTouchListener(this)
+        gDetector = GestureDetector(this, this)
     }
+
+    override fun onTouch(v:View?, event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN){
+            text.text = "手指按下"
+        }
+        if (event?.action == MotionEvent.ACTION_UP){
+            text.text = "手指彈開"
+        }
+        gDetector.onTouchEvent(event)
+        return true
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        text.text = "拖曳"
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
 }
